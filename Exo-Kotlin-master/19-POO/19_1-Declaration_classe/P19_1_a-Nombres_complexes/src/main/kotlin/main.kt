ca7f7c46d79975004f2cef7b53343926f01b216b
UgoFaye <faye.ugo77@gmail.com>
2024-09-23 16:14:05 +0200

    1.0st commit

--- Exo-Kotlin-master/19-POO/19_1-Declaration_classe/P19_1_a-Nombres_complexes/src/main/kotlin/main.kt
+++ Exo-Kotlin-master/19-POO/19_1-Declaration_classe/P19_1_a-Nombres_complexes/src/main/kotlin/main.kt
@@ -1,8 +1,7 @@
-// write the class Complex here
-
+class Complex(var real : Double, var image : Double)
 
 fun main() {
-    val number1 = Complex()
+    val number1 = Complex(0.0,0.0)
     number1.real = 5.0
     number1.image = 2.0
     println(number1.real)
