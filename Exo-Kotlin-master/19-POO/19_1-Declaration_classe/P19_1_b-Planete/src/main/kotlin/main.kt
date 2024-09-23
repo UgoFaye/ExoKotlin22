791e5214cf05cfb9898bbf57cc28df2ba9345ec7
UgoFaye <faye.ugo77@gmail.com>
2024-09-23 16:57:31 +0200

    2nd commit 23sept

--- Exo-Kotlin-master/19-POO/19_1-Declaration_classe/P19_1_b-Planete/src/main/kotlin/main.kt
+++ Exo-Kotlin-master/19-POO/19_1-Declaration_classe/P19_1_b-Planete/src/main/kotlin/main.kt
@@ -1,9 +1,11 @@
-// write your code here
-
+class Planet(var star : String, var mass : Double, var numberOfSatellites : Int)
 
 fun main() {
-    val kotlinPlanet = Planet()
+    val kotlinPlanet = Planet("Unknown Star", 1.0,1)
     kotlinPlanet.star = "Java"
     kotlinPlanet.mass = 2.0
     kotlinPlanet.numberOfSatellites = 0
+    println(kotlinPlanet.star)
+    println(kotlinPlanet.mass)
+    println(kotlinPlanet.numberOfSatellites)
 }
--- Exo-Kotlin-master/19-POO/19_1-Declaration_classe/P19_1_c-Surface/src/main/kotlin/main.kt
+++ Exo-Kotlin-master/19-POO/19_1-Declaration_classe/P19_1_c-Surface/src/main/kotlin/main.kt
@@ -3,13 +3,14 @@ class Rectangle {
     var height: Int = 0
 }
 
-fun printArea(rectangle: Rectangle) {
-    // ...
+fun printArea(rectangle: Rectangle): Int {
+    var Area = rectangle.width * rectangle.height
+    return Area
 }
 
 fun main() {
-    val rectlangle = Rectangle()
-    rectlangle.width = 5
-    rectlangle.height = 3
-    printArea(rectlangle)
+    val rectangle = Rectangle()
+    rectangle.width = 5
+    rectangle.height = 3
+    println(printArea(rectangle))
 }
